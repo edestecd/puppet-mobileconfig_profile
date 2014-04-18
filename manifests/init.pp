afdf0202de585c46235cd47de08b53950bc3234d
Chris Edester <edestecd@miamioh.edu>
2014-04-18 00:52:42 -0400

    Also manage the directory and file

---
+++ manifests/init.pp
@@ -0,0 +1,54 @@
+# init.pp
+# Manage both the file resource and config_profile resource for an
+# OS X configuration profile (mobileconfig)
+#
+
+define mobileconfig_profile (
+  $identifier    = $title,
+  $ensure        = $mobileconfig_profile::params::ensure,
+  $profiles_path = $mobileconfig_profile::params::profiles_path,
+  $path          = $mobileconfig_profile::params::path,
+  $system        = $mobileconfig_profile::params::system,
+  $user          = undef,
+  $home          = undef,
+  $content       = undef,
+  $source        = undef,
+) {
+
+  include ::mobileconfig_profile::params
+
+  validate_absolute_path($profiles_path)
+  validate_absolute_path($path)
+  validate_bool($system)
+
+  if !defined(File[$profiles_path]) {
+    file { $profiles_path:
+      ensure => directory,
+      mode   => 0755,
+      owner  => $mobileconfig_profile::params::root_user,
+      group  => $mobileconfig_profile::params::root_group,
+    }
+  }
+
+  if !defined(File[$path]) {
+    file { $path:
+      ensure  => file,
+      mode    => 0644,
+      owner   => $mobileconfig_profile::params::root_user,
+      group   => $mobileconfig_profile::params::root_group,
+      content => $content,
+      source  => $source,
+    }
+  }
+
+  config_profile { $title:
+    identifier => $identifier,
+    ensure     => $ensure,
+    path       => $path,
+    system     => $system,
+    user       => $user,
+    home       => $home,
+    subscribe  => File[$path],
+  }
+
+}
---
+++ manifests/params.pp
@@ -0,0 +1,21 @@
+# params.pp
+# Set up configuration profile parameters defaults etc.
+#
+
+class mobileconfig_profile::params {
+
+  # init.pp
+  # At least OSX 10.8 "Mountain Lion"
+  if ($::osfamily == 'Darwin') and ($::macosx_productversion >= 10.8.0) {
+    $ensure        = installed
+    $profiles_path = "/var/lib/puppet/mobileconfigs"
+    $path          = "${profiles_path}/${identifier}"
+    $system        = false
+
+    $root_user     = 'root'
+    $root_group    = 'wheel'
+  } else {
+    fail("The ${module_name} module is not supported on a ${::osfamily} based system with version ${::macosx_productversion}.")
+  }
+
+}
